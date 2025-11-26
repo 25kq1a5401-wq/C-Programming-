e4e0647dfbfde73f5809aec401e2a9ee013f0dd9
25kq1a5401-wq <25kq1a5401@pace.ac.in>
2025-11-26 20:40:51 +0530

    Update Even Odd using functions.c

--- Even Odd using functions.c
+++ Even Odd using functions.c
@@ -3,18 +3,18 @@ void checkEvenOdd(int num)
 {
   if(num%2==0)   
   {
-    printf("%d is an even number:\n",num);
+    printf("%d is an even number.\n",num);
   }
   else
   {
-    printf("%d is an odd number:\n",num);
+    printf("%d is an odd number.\n",num);
   }         
 }
 int main()
 {
   int number;
   printf("Enter an integer:");
-  scanf("%d" &number);
+  scanf("%d",&number);
   checkEvenOdd(number);
   return 0;
 }  
