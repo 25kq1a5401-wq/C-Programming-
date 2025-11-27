8c5db7a67918c1640c1e3beb0a6eeb2e4ab951dd
25kq1a5401-wq <25kq1a5401@pace.ac.in>
2025-11-27 20:45:27 +0530

    Create low to up rec.c

---
+++ low to up rec.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int lower,upper;
+  void printnum(int,int);
+  scanf("%d%d",&lower,&upper);
+  printnum(lower,upper);
+}
+void printnum(int lower,int upper)
+{
+   printf("%d ",lower);
+   lower++;
+   if(lower>upper)
+   exit(0);
+}     
