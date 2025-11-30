47770db632d5919871ab968c31b5803291e4a970
25kq1a5401-wq <25kq1a5401@pace.ac.in>
2025-11-30 19:12:09 +0530

    Create String length.c

---
+++ String length.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+      char name[30];
+      int length;
+      printf("Enter the name");
+      gets(name);
+      printf("length is %d",strlen(name));
+}      
