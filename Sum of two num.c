0b4192f862d0de597aeca996ad1619da339d5340
25kq1a5401-wq <25kq1a5401@pace.ac.in>
2025-12-04 22:24:13 +0530

    Create Sum of two num.c

---
+++ Sum of two num.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+   int n1,n2;
+   clrscr();
+   printf("enter 1st number:\n");
+   scanf("%d",&n1);
+   printf("enter 2nd number:\n");
+   scanf("%d",&n2);
+   printf("total=%d",n1+n2);
+   getch();
+}   
