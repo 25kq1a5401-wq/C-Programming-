8fb92c408afc2b9b303c0157ed3e7d50b4e7f4cb
25kq1a5401-wq <25kq1a5401@pace.ac.in>
2025-12-17 08:04:20 +0530

    Create Leap year.c

---
+++ Leap year.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+void main()
+{
+    int year;
+    printf("enter a year:\n");
+    scanf("%d",&year);
+    if(((year%4==0)&&((year%400==0)||(year%100!==0))
+    {
+         printf("%d is a leap year",&year);
+    }
+    else
+    { 
+         printf("%d is not a leap year ,&year");
+    }
+    getch();
+}         
